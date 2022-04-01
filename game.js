ea29100e88816c1e7b26176e8a1b2fc6a2e9b2aa
Garrett Franklin <garrettfranklin@pm.me>
2022-03-31 20:43:21 -0600

    Finish the game logic
    Add functionality to update the scoreboard. Add a check to the round()
    function that determines if either player has reached a score of 3.
    If check determines game is over, display "Game Over." and render a
    button to reset the game.

--- game.js
+++ game.js
@@ -3,15 +3,47 @@ function computerPlay() {
   const moves = ["rock", "paper", "scissors"];
   return moves[Math.floor(Math.random() * 3)]
 }
+// Update the scoreboard after every round.
+function updateScore(outcome) {
+  const playerScoreDisplay = document.querySelector("#player-score");
+  const computerScoreDisplay = document.querySelector("#computer-score");
+  let playerScore = Number(playerScoreDisplay.textContent);
+  let computerScore = Number(computerScoreDisplay.textContent);
+  if (outcome === "tie") {
+    return
+  } else if (outcome === "win") {
+    playerScoreDisplay.textContent = ++playerScore;
+  } else {
+    computerScoreDisplay.textContent = ++computerScore;
+  }
+}
+// Create a reset button to be rendered when the game is over
+const resetButton = document.createElement("button");
+resetButton.setAttribute("id", "reset-button");
+resetButton.textContent = "Play Again";
+// Create a reset function
+function reset() {
+  document.querySelectorAll(".score").forEach(score => score.textContent = "0");
+  document.querySelector("#reset-button").remove();
+}
+resetButton.addEventListener("click", reset);
 // Determine who won a round, return the result
 function round(playerSelection, computerSelection) {
+  const playerCheckScore = Number(document.querySelector("#player-score").textContent) === 3;
+  const computerCheckScore = Number(document.querySelector("#computer-score").textContent) === 3;
+
+  if (playerCheckScore || computerCheckScore) {
+    document.querySelector("#display").textContent = "Game Over.";
+    document.querySelector("#display-container").appendChild(resetButton);
+    return;
+  }
+
   const heroGuess = playerSelection.toLowerCase();
 
   function displayResults(hero, computer, result) {
     const display = document.querySelector("#display");
-    display.textContent = `${result} - You play: ${hero}, opponent plays: ${computer}`
-  }
-  
+    display.textContent = `${result} - You play: ${hero}, opponent plays: ${computer}`;
+  }  
   if (heroGuess === computerSelection) {
     const result = "tie";
     displayResults(heroGuess, computerSelection, result);
@@ -46,19 +78,6 @@ function round(playerSelection, computerSelection) {
       displayResults(heroGuess, computerSelection, result);
       updateScore(result);
     }
-  }
-}
-function updateScore(outcome) {
-  const playerScoreDisplay = document.querySelector("#player-score");
-  const computerScoreDisplay = document.querySelector("#computer-score");
-  let playerScore = Number(playerScoreDisplay.textContent);
-  let computerScore = Number(computerScoreDisplay.textContent);
-  if (outcome === "tie") {
-    return
-  } else if (outcome === "win") {
-    playerScoreDisplay.textContent = ++playerScore;
-  } else {
-    computerScoreDisplay.textContent = ++computerScore;
   }
 }
 function startRound(e) {
